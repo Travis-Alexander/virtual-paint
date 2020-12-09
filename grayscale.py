5b7914eff2d57bb41d47501a1142cd59e84dff6f
Travis-Alexander <72216997+Travis-Alexander@users.noreply.github.com>
2020-12-09 11:30:38 -0600

    Fixed grayscale picture pull up

--- grayscale.py
+++ grayscale.py
@@ -1,9 +1,14 @@
 import cv2
 
-img = cv2.imread("Images/timmy.jpg")
+
+# Code below this line is a grayscale image
+
+img = cv2.imread("Images/timmy.jpg") #learn to load gifs 
 
 imgGray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 
 cv2.imshow("Gray image", imgGray)
 
 cv2.waitKey(0)
+
+#Code Above this line is a grayscale image
