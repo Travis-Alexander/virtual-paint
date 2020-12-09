006d673c9367d9cfbce347ecfcb4b87be255ea69
Travis-Alexander <72216997+Travis-Alexander@users.noreply.github.com>
2020-12-09 13:11:03 -0600

    canny image

--- grayscale.py
+++ grayscale.py
@@ -6,9 +6,14 @@ import cv2
 img = cv2.imread("Images/timmy.jpg") #learn to load gifs 
 
 imgGray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+imgBlur = cv2.GaussianBlur(imgGray, (7,7),0)  # Has to have odd numbers
+imgCanny = cv2.Canny(img, 100, 100) # shows edges 
+
+
 
 cv2.imshow("Gray image", imgGray)
-
+cv2.imshow("Blur image", imgBlur)
+cv2.imshow("Canny image", imgCanny)
 cv2.waitKey(0)
 
 #Code Above this line is a grayscale image
