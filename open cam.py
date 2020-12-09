fd185d5080b1a69f011b74faad291ae7a63c5a9b
Travis-Alexander <72216997+Travis-Alexander@users.noreply.github.com>
2020-12-09 08:18:16 -0600

    open cam

---
+++ open cam.py
@@ -0,0 +1,25 @@
+import cv2
+
+cap = cv2.VideoCapture(0)
+cap.set(3, 640)  #id number is 3 for width
+cap.set(4, 480)  #id number is 4 for height
+cap.set(10 ,100) #id number is 10 for brightness of camera
+
+
+
+
+
+while True:
+    success, img = cap.read()
+    cv2.imshow("Video", img)
+    if cv2.waitKey(1) & 0xFF ==ord('q'):
+        break
+
+
+# img = cv2.imread("Images/timmy.gif")
+
+# imgGray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+
+# cv2.imshow("GRay image", imgGray)
+
+# cv2.waitKey(0)
