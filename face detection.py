9ee6549f6c62acfd7b9a14b851443271b500027f
Travis Alexander <talexander21@basecampcodingacademy.org>
2020-12-13 22:47:24 -0600

    face detection built

---
+++ face detection.py
@@ -0,0 +1,15 @@
+import cv2
+import numpy as np
+
+faceCascade = cv2.CascadeClassifier("Images/haarcascade_frontalface_default.xml")
+img = cv2.imread("Images/charles.jpg")
+imgGray = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
+
+faces = faceCascade.detectMultiScale(imgGray, 1.1, 4)
+for (x,y,w,h) in faces:
+    cv2.rectangle(img, (x,y), (x+w, y+h), (255,0,0), 2)
+    
+
+imgResize = cv2.resize(img, (640,680))
+cv2.imshow("Result", imgResize)
+cv2.waitKey(0)
