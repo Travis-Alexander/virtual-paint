6cff41c3a47d136ef3c5a8dc865cebe80b802620
Travis-Alexander <72216997+Travis-Alexander@users.noreply.github.com>
2021-03-03 09:28:58 -0600

    webcam

--- tutorial stuff/open cam.py
+++ tutorial stuff/open cam.py
@@ -1,6 +1,6 @@
 import cv2
 
-cap = cv2.VideoCapture(0)
+cap = cv2.VideoCapture(1)
 cap.set(3, 640)  #id number is 3 for width
 cap.set(4, 480)  #id number is 4 for height
 cap.set(10 ,100) #id number is 10 for brightness of camera
